916d3d6c85245ab7c1b80d08aa7c8d9d934b9323
LukyTO <mehdi.aouir1@gmail.com>
2023-02-02 13:38:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React from "react";
+import Navbar from "./Navbar";
+import About from "./About"
+import Work from "./Work";
+import Cards from "./Cards";
+import Contact from "./Contact";
+import data from "./data";
+import Services from "./Services";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+
+export default function App() {
+
+    const cards = data.map(item => {
+        return (
+            <Cards
+                key={item.id}
+                {...item}
+            />
+        )
+    })
+
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 3
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1800 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1800, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+
+
+    return (
+        <div>
+            <Navbar />
+            <About />
+            <Work />
+            <Services />
+            <Carousel
+                responsive={responsive}
+                draggable={true}
+                autoPlay={true}
+                infinite={true}
+                showDots={true}
+                autoPlaySpeed={3000}
+                className="card-list">
+                {cards}
+            </Carousel>
+            <Contact />
+        </div>
+    )
+}
---
+++ src/Services.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Services() {
+    return(
+        <div className="services">
+            <h1 className="services-title">Services</h1>
+        </div>
+    )
+}
