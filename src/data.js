1e39d03d05e864c5b53464347229ecb172c88754
LukyTO <mehdi.aouir1@gmail.com>
2023-03-01 14:27:41 +0100

    add

--- src/data.js
+++ src/data.js
@@ -5,43 +5,51 @@ import imageFour from "./image/development.jpg"
 import imageFive from "./image/content-creator.jpg"
 import imageSix from "./image/illustration.jpg"
 
+const images = [
+    { id: "concept-art", src: imageFirst },
+    { id: "chara-design", src: imageSecond },
+    { id: "animation", src: imageThird },
+    { id: "development", src: imageFour },
+    { id: "content-creator", src: imageFive },
+    { id: "illustration", src: imageSix },
+]
+
 
 export default [
     {
-        id:1,
+        id: 1,
         title: "Concept Art",
         description: "",
-        coverImg: imageFirst,
+        coverImg: images.find((i) => i.id === "concept-art").src,
     },
     {
-        id:2,
+        id: 2,
         title: "Character Design",
         description: "",
-        coverImg: imageSecond,
+        coverImg: images.find((i) => i.id === "chara-design").src,
     },
     {
-        id:3,
+        id: 3,
         title: "Animation",
         description: "",
-        coverImg: imageThird,
+        coverImg: images.find((i) => i.id === "animation").src,
     },
     {
-        id:4,
+        id: 4,
         title: "Web Development",
         description: "",
-        coverImg: imageFour,
+        coverImg: images.find((i) => i.id === "development").src,
     },
     {
-        id:5,
+        id: 5,
         title: "Content Creator",
         description: "",
-        coverImg: imageFive,
+        coverImg: images.find((i) => i.id === "content-creator").src,
     },
     {
-        id:6,
+        id: 6,
         title: "Illustration",
         description: "",
-        coverImg: imageSix,
+        coverImg: images.find((i) => i.id === "illustration").src,
     },
-    
 ]
