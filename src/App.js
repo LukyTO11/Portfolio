1869318ce09d0ca1d6ad6ab0a1f59e25c8344bc7
LukyTO <mehdi.aouir1@gmail.com>
2023-03-02 18:28:50 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import Cards from "./Cards";
 import Contact from "./Contact";
 import data from "./data";
 import Services from "./Services";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 
 
 export default function App() {
@@ -22,53 +20,28 @@ export default function App() {
         document.body.classList.toggle("dark")
     }
 
-    const cards = data.map(item => {
+    const cards = data.map((item) => {
         return (
             <Cards
                 key={item.id}
-                {...item}
+                title={item.title}
+                description={item.description}
+                className={item.className}
+                coverImg={item.coverImg}
             />
         )
     })
-
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 3
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1800 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1800, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
 
 
 
     return (
         <div>
-            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} toggleBodyClass={toggleBodyClass}/>
-            <About darkMode={darkMode}/>
-            <Work darkMode={darkMode}/>
-            <Services darkMode={darkMode}/>
-            <Carousel
-                responsive={responsive}
-                draggable={false}
-                autoPlay={true}
-                infinite={true}
-                showDots={true}
-                autoPlaySpeed={6000}
-                className="card-list">
-                {cards}
-            </Carousel>
-            <Contact darkMode={darkMode}/>
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} toggleBodyClass={toggleBodyClass} />
+            <About darkMode={darkMode} />
+            <Work darkMode={darkMode} />
+            <Services darkMode={darkMode} />
+            <div className="cards-list">{cards}</div>
+            <Contact darkMode={darkMode} />
         </div>
     )
 }
