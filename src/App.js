b66b748a62df96420938b1e55df258c01d57f9a3
LukyTO <mehdi.aouir1@gmail.com>
2023-03-17 19:46:36 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Home from "./Home"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import SeeMyWorks from "./components/SeeMyWorks";
 
-export default function App() { 
+export default function App() {
 
     return (
         <BrowserRouter>
             <div>
                 <Routes>
-                    <Route path="/Portfolio" element={<Home />} />
-                    <Route path="/see-my-works" element={<SeeMyWorks/>} />
+                    <Route path="/Portfolio" element={<Home/>} />
+                    <Route path="/see-my-works" element={<SeeMyWorks />} />
                 </Routes>
             </div>
         </BrowserRouter>
